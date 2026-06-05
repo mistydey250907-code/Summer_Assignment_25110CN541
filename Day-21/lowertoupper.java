6f426ba93ae7fd296a3175cd2bcb96162f1c58f9
Misty Dey <mistydey250907@gmail.com>
2026-06-05 09:03:39 +0530

    Remove Day-10 through Day-19 folders

--- Day-21/lowertoupper.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class lowertoupper {
-    public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-     System.out.print("Enter a string: ");
-        String str = sc.nextLine();
-     System.out.println("String after converting lower to upper case: "+str.toUpperCase());
-    }
-}
