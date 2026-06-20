53df3589f9c22e49f647c175c6667682850f1d90
Misty Dey <mistydey250907@gmail.com>
2026-06-21 00:20:58 +0530

    Day-21

---
+++ Week-3/Day-21/strlen.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class strlen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int length = str.lastIndexOf(""); 
+        
+        System.out.println("The length of the string is: " + length);
+
+    }
+}
