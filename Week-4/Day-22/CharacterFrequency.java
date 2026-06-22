0e141ae06bbe6699dc02918172d48b82a1a3899f
Misty Dey <mistydey250907@gmail.com>
2026-06-22 08:43:22 +0530

    Day-22

---
+++ Week-4/Day-22/CharacterFrequency.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CharacterFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        int[] frequency = new int[256];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            frequency[ch]++;
+        }
+
+        System.out.println("Character frequencies:");
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (frequency[ch] != 0) {
+                System.out.println("'" + ch + "' comes " + frequency[ch] + " times");
+                frequency[ch] = 0;
+            }
+        }
+
+        sc.close();
+    }
+}
